24206caf8213950cdb9f081687446d0011b39a06
Ian Davenport <davenporti632@gmail.com>
2021-02-23 11:15:58 +0000

    First commit

--- iOSDiceGame/ViewController.swift
+++ iOSDiceGame/ViewController.swift
@@ -1,7 +1,6 @@
-//
+
 //  ViewController.swift
 //  iOSDiceGame
-//
 //  Created by Ian Davenport on 23/02/2021.
 //
 
@@ -11,9 +10,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
-}
 
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
